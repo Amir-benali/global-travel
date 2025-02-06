bdcfbb68d2e68b42fbb44ca0f286dfe1f0742e9a
Amir-benali <benaliamir77@gmail.com>
2025-02-06 14:25:05 +0100

    added datasource and Iservice

---
+++ src/main/java/com/globalTravel/tests/Main.java
@@ -0,0 +1,10 @@
+package com.globalTravel.tests;
+
+import com.globalTravel.utils.DataSource;
+
+public class Main {
+    public static void main(String[] args) {
+        //connection test
+        DataSource ds= DataSource.getInstance();
+    }
+}
