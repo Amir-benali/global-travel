ed52153b08a509db0df4ecbc7777408760b6bb8b
AzizX25 <110631215+AzizX25@users.noreply.github.com>
2025-02-07 23:52:16 +0100

    Add files via upload

--- src/main/java/com/globalTravel/tests/Main.java
+++ src/main/java/com/globalTravel/tests/Main.java
@@ -1,21 +1,22 @@
-package globaltravel.tests;
+package com.globalTravel.tests;
 
-import globaltravel.models.*;
-import globaltravel.services.AirlineService;
-import globaltravel.services.FlightService;
-import globaltravel.services.TicketService;
+
+import com.globalTravel.models.*;
+import com.globalTravel.services.AirlineService;
+import com.globalTravel.services.FlightService;
+import com.globalTravel.services.TicketService;
 
 public class Main {
     public static void main(String[] args) {
 
         AirlineService as=new AirlineService();
-        //.ajouter(new Airline("Tunisia", "TUN", "Tunisair"));
+        //as.ajouter(new Airline("Tunisia", "TUN", "Tunisair"));
 
         FlightService fs=new FlightService();
         //fs.ajouter(new Flight("F1", 1, "TUN", "CDG", "2021-12-12 12:00:00", "2021-12-12 14:00:00", 2, 100, 200.0, FlightStatus.Scheduled));
 
         TicketService ts=new TicketService();
-        //ts.ajouter(new Ticket(2, "A1", TicketClass.Business, 100.0,TicketStatus.Not_Booked, "2021-12-12 12:00:00"));
+        //ts.ajouter(new Ticket(1, "A1", TicketClass.Business, 100.0, TicketStatus.Not_Booked, "2021-12-12 12:00:00"));
 
         //as.modifier(new Airline(1, "Tunisair", "TUN", "Tunisia"));
         //fs.modifier(new Flight(2, "F123", 1, "TUN", "CDG", "2021-12-12 12:00:00", "2021-12-12 14:00:00", 2, 100, 200.0, FlightStatus.Scheduled));
@@ -27,5 +28,6 @@ public class Main {
 
         //fs.supprimer(new Flight(2, "",1, "", "", "", "", 0, 0, 0.0, FlightStatus.Scheduled));
         //as.supprimer(new Airline(1, "", "", ""));
+        //ts.supprimer(new Ticket(1, 2, "Abc251", TicketClass.Business, 100.0,TicketStatus.Not_Booked, "2021-12-12 12:00:00"));
     }
 }
