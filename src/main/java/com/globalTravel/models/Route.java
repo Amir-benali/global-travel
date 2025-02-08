89536fdeff466c12471ba87ec2e35722247b74f9
Amir-benali <benaliamir77@gmail.com>
2025-02-08 15:24:36 +0100

    added route service

--- src/main/java/com/globalTravel/models/Route.java
+++ src/main/java/com/globalTravel/models/Route.java
@@ -1,15 +1,16 @@
 package com.globalTravel.models;
 
-import java.util.Date;
+
+import java.time.LocalDateTime;
 
 public class Route {
     private int id;
-    private Date date_start;
-    private Date date_destination;
+    private LocalDateTime date_start;
+    private LocalDateTime  date_destination;
     private String location_start;
     private String location_destination;
 
-    public Route(int id, Date date_start, Date date_destination, String location_start, String location_destination) {
+    public Route(int id, LocalDateTime  date_start, LocalDateTime  date_destination, String location_start, String location_destination) {
         this.id = id;
         this.date_start = date_start;
         this.date_destination = date_destination;
@@ -17,7 +18,7 @@ public class Route {
         this.location_destination = location_destination;
     }
 
-    public Route(Date date_start, Date date_destination, String location_start, String location_destination) {
+    public Route(LocalDateTime  date_start, LocalDateTime  date_destination, String location_start, String location_destination) {
         this.date_start = date_start;
         this.date_destination = date_destination;
         this.location_start = location_start;
@@ -32,19 +33,19 @@ public class Route {
         this.id = id;
     }
 
-    public Date getDate_start() {
+    public LocalDateTime  getDate_start() {
         return date_start;
     }
 
-    public void setDate_start(Date date_start) {
+    public void setDate_start(LocalDateTime  date_start) {
         this.date_start = date_start;
     }
 
-    public Date getDate_destination() {
-        return date_destination;
+    public LocalDateTime  getDate_destination() {
+        return  date_destination;
     }
 
-    public void setDate_destination(Date date_destination) {
+    public void setDate_destination(LocalDateTime  date_destination) {
         this.date_destination = date_destination;
     }
 
--- src/main/java/com/globalTravel/tests/PrivateCarMain.java
+++ src/main/java/com/globalTravel/tests/PrivateCarMain.java
@@ -2,11 +2,18 @@ package com.globalTravel.tests;
 
 import com.globalTravel.models.CarDriver;
 import com.globalTravel.models.PrivateCar;
+import com.globalTravel.models.Route;
 import com.globalTravel.services.PrivateCarService;
+import com.globalTravel.services.RouteService;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class PrivateCarMain {
     public static void main(String[] args) {
-        PrivateCarService service = new PrivateCarService();
+        RouteService service = new RouteService();
         CarDriver driver1 =new CarDriver(3,"ahmed","amin","99885544");
 //        service.ajouter(new CarDriver("ahmed","amin","99885544"));
 //        service.ajouter(new PrivateCar("brand 1","model 1",3));
@@ -17,7 +24,10 @@ public class PrivateCarMain {
 //        service.ajouter(new PrivateCar("brand 4","model 4",1));
 //        service.ajouter(new PrivateCar("brand 5","model 5",4));
 //        service.supprimer(new CarDriver(2,"aziz","amin","999800815"));
-        service.ajouter(new PrivateCar("brand 5","model 5",4 ,driver1));
+//        service.ajouter(new Route(1,LocalDateTime.now() , LocalDateTime.of(2025,2,25,19,25,25),"1115","1209"));
+
+        service.supprimer(new Route(2,LocalDateTime.now() , LocalDateTime.of(2025,2,25,19,25,25),"1115","1209"));
+
         System.out.println(service.rechercher());
     }
 }
